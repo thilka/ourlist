f9c41f65490c079a4099c0162eb1e81b141bfd8e
Tobias Hilka <tobias.hilka@nexusgroup.com>
2017-07-19 15:39:43 +0200

    items are now read from firebase instead of dummy data.

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -6,15 +6,12 @@ import {
   TouchableHighlight,
   Text,
   Alert, 
-  View
+  View,
+  ActivityIndicator
 } from 'react-native';
 
 import { CheckBox } from 'react-native-elements'
-
-const items = [
-  {name: 'Item 1'},
-  {name: 'Item 2'},
-];
+import firebase from '../firebase/FirebaseConfig'
 
 export default class Details extends Component {
 
@@ -23,6 +20,40 @@ export default class Details extends Component {
     headerStyle: { backgroundColor: '#58a2fb'},
     headerTintColor: 'black',
   })
+
+  constructor() {
+    super();
+    this.ref = null;
+    this.state = {
+      items: null,
+      loading: true
+    }
+  }
+
+  // On mount, subscribe to ref updates
+  componentDidMount() {
+    const {item} = this.props.navigation.state.params
+
+    this.ref = firebase.database().ref('items');
+    this.ref.equalTo(item.id).orderByChild('project').on('value', this.handleUpdates)
+  }
+
+  // On unmount, ensure we no longer listen for updates
+  componentWillUnmount() {
+    if (this.ref) {
+      this.ref.off('value', this.handleUpdates);
+    }
+  }
+
+  // Bind the method only once to keep the same reference
+  handleUpdates = (snapshot) => {
+    updatedItems = []
+
+    snapshot.forEach((item) => {
+      updatedItems.push({id: item.key, name: item.val().name })
+    });
+    this.setState({items: updatedItems, loading: false})
+  }
 
   renderItem = ({item, index}) => {
     return (
@@ -34,12 +65,17 @@ export default class Details extends Component {
 
   render() {
     return (
-      <View style={styles.detailsPane}>
-        <FlatList 
-          data={items} 
-          keyExtractor={(item) => item.name} 
-          renderItem={this.renderItem} />
-      </View>
+      this.state.loading ? 
+        <ActivityIndicator color='#000000' size='large' /> : 
+        (
+          <View style={styles.detailsPane}>
+            <FlatList 
+              data={this.state.items}
+              extraData={this.state} 
+              keyExtractor={(item) => item.name} 
+              renderItem={this.renderItem} />
+          </View>
+        )
     )
   }
 }
@@ -67,6 +103,4 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     flex:1
   }
-  
-
 });
